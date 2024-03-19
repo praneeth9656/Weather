68812c7ef65fb3d2e6d01bbdd8e2cbacea5bf69a
PraneethKumar_Gangireddy <praneethkumarsmiles@gmail.com>
2024-03-19 17:31:40 -0500

    changes in front end file

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Weathercard from './components/weather';
 function App() {
   return (
     <div className="App">
+    <h1 className='Heading'>Weather </h1>
       <Weathercard/>
     </div>
   );
