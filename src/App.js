261ea22a43c91a33d4cee03a7bd51df8cc98bc58
PraneethKumar_Gangireddy <praneethkumarsmiles@gmail.com>
2024-03-19 21:57:59 -0500

    Testing for Multiple pipeline triggers

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Weathercard from './components/weather';
 function App() {
   return (
     <div className="App">
-    <h1 className='Heading'>Weather </h1>
+    <h1 className='Heading'>Weather App</h1>
       <Weathercard/>
     </div>
   );
