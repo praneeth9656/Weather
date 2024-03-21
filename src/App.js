dfdf2be22cb100d7c706bd628f7b25236643c1c3
PraneethKumar_Gangireddy <praneethkumarsmiles@gmail.com>
2024-03-21 16:16:33 -0500

    March 21 commit2

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Weathercard from './components/weather';
 function App() {
   return (
     <div className="App">
-    <h1 className='Heading'>Weather App 1</h1>
+    <h1 className='Heading'>Weather App 2</h1>
       <Weathercard/>
     </div>
   );
